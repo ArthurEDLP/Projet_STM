1e95d5729c11e743f68fc786675745049d0aa386
PAPEIX <thomaspapeix@MacBook-Air-de-PAPEIX.local>
2026-01-23 11:19:15 +0100

    gaaga

--- code.R
+++ code.R
@@ -1,5 +1,26 @@
+
+### ------------------------------------------------------------------
+# Chargement des librairies
+
+library(dplyr)
+
+### ------------------------------------------------------------------
+# Importation des données
+
 # On est en dollars
 
-gold <- read.csv2("data/donnees_mensuel_or_90_25.csv", sep=",") 
-gold[c("Ouv", "X.Plus.Haut", "Plus.Bas")] <- NULL
+gold <- read.csv2("data/OR.csv", sep=",") 
 
+gold[c("Ouv.", "X.Plus.Haut", "Plus.Bas")] <- NULL
+
+gold <- as.data.frame(gold)
+
+gold <- gold |>
+  rename(
+    Volume = `Vol.`,
+    Variation_pct = `Variation..`
+  )
+
+
+
+hfhghg
