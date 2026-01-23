c03c11b51a1f4359042cf8661fd317fe3bb34494
Arthur <arthur.edlp@outlook.fr>
2026-01-23 10:30:53 +0100

    changement noms des colonnes

--- code.R
+++ code.R
@@ -1,6 +1,24 @@
-# test collaboration
+### ------------------------------------------------------------------
+# Chargement des librairies
+
+library(dplyr)
+
+### ------------------------------------------------------------------
+# Importation des données
+
 # On est en dollars
 
 gold <- read.csv2("data/donnees_mensuel_or_90_25.csv", sep=",") 
-ghfj
-gold[c("Ouv", "X.Plus.Haut", "Plus.Bas")] <- NULL
+
+gold[c("Ouv.", "X.Plus.Haut", "Plus.Bas")] <- NULL
+
+gold <- as.data.frame(gold)
+
+gold <- gold |>
+  rename(
+    Volume = `Vol.`,
+    Variation_pct = `Variation..`
+  )
+
+
+
